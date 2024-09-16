6e24482a561a9b66da1639b203a37ab2d00f2d4c
étienne <etienne.millerioux@studi.fr>
2024-09-16 11:50:20 +0200

    handle query with manually set filters

--- CodeSharpDoc/services/rag_service.py
+++ CodeSharpDoc/services/rag_service.py
@@ -88,12 +88,16 @@ class RAGService:
             question = questionAnalysis.translated_question
 
         #RAG documents retrieval
-        filters = {
-            "$and": [
-                {"functional_type": "Controller"},
-                {"summary_kind": "method"}
-            ]
-        }
+        filters = {}
+        if RAGService.has_filters(question):
+            filters, question = RAGService.extract_filters(question)
+        else:
+            filters = {
+                "$and": [
+                    {"functional_type": "Controller"},
+                    {"summary_kind": "method"}
+                ]
+            }
         retrieved_chunks = langchain_rag.retrieve(self.llm, self.vectorstore, question, additionnal_context, filters, give_score, 10, 0.2, 2)
         
         #BM25 retrieval
@@ -147,3 +151,43 @@ class RAGService:
                 raise ValueError(f"Unhandled operator: {operator}")
 
         return False
+    
+    def has_filters(question: str) -> bool:
+        return  question.__contains__("filters:") or question.__contains__("filtres :")
+    
+    def extract_filters(question: str) -> Tuple[dict, str]:
+        filters = {}
+        if question.__contains__("filters:"):
+            filters_str = question.split("filters:")[1]
+            question = question.split("filters:")[0]
+        elif question.__contains__("filtres :"):
+            filters_str = question.split("filtres :")[1]
+            question = question.split("filtres :")[0]
+        filters_str = filters_str.strip()
+        filters = RAGService.get_filters_from_str(filters_str)
+        return filters, question
+    
+    def get_filters_from_str(filters_str: str) -> dict:
+        filters = []
+        filters_list = filters_str.lower().split(',')
+
+        for filter in filters_list:
+            # functional_type: Controller, Service, Repository, ...
+            if "controller" in filter or "service" in filter or "repository" in filter:
+                # Ensure the first letter is uppercase
+                functional_type = filter.strip().capitalize()
+                filters.append({"functional_type": functional_type})
+            # summary_kind: class, method, (property, enum_member), ...
+            elif "method" in filter or "méthode" in filter:
+                filters.append({"summary_kind": "method"})
+            elif "class" in filter:
+                filters.append({"summary_kind": "class"})
+
+        # If there are more than one filters, wrap in "$and", otherwise return a single filter
+        if len(filters) > 1:
+            return {"$and": filters}
+        elif len(filters) == 1:
+            return filters[0]  # Just return the single condition directly
+        else:
+            return {}  # Return an empty filter if no conditions
+
