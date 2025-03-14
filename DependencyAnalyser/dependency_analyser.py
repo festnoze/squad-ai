e66d0e36aedd9ca832afff048ce0e908ddc6ebfb
étienne <etienne.millerioux@studi.fr>
2025-03-15 00:30:39 +0100

    cline 2

--- DependencyAnalyser/dependency_analyser.py
+++ DependencyAnalyser/dependency_analyser.py
@@ -101,33 +101,293 @@ class DependencyAnalyzer:
             'by_external_deps': dict(external_dep_groups)
         }
     def partition_by_granularity(self, granularity: int) -> None:
-        undirected = self.dependency_graph.to_undirected()
-        communities = list(nx.algorithms.community.greedy_modularity_communities(undirected))
-        final_groups: List[Set[str]] = []
-        for c in communities:
-            final_groups.extend(self.split_community(c, granularity))
-        idx = 1
-        for g in final_groups:
-            for m in g:
-                self.module_to_group[m] = idx
-            idx += 1
+        """
+        Divise les modules en exactement 'granularity' sous-librairies si possible,
+        en utilisant une combinaison de critères (dépendances internes, externes, hiérarchie).
+        
+        Args:
+            granularity: Nombre cible de sous-librairies à créer
+        """
+        if granularity < 1:
+            granularity = 1
+        
+        all_modules = list(self.dependency_graph.nodes())
+        if len(all_modules) <= granularity:
+            # Si le nombre de modules est inférieur ou égal à la granularité,
+            # chaque module forme sa propre sous-librairie
+            for idx, module in enumerate(all_modules, 1):
+                self.module_to_group[module] = idx
+        else:
+            # Plusieurs stratégies pour créer les groupes
+            strategies = [
+                self._group_by_internal_deps,
+                self._group_by_external_deps,
+                self._group_by_hierarchy
+            ]
+            
+            # Évaluer chaque stratégie et sélectionner celle qui donne le meilleur équilibre
+            best_strategy = None
+            best_score = float('inf')
+            
+            for strategy in strategies:
+                groups, score = strategy(granularity)
+                if score < best_score:
+                    best_score = score
+                    best_strategy = groups
+            
+            # Appliquer la meilleure stratégie
+            idx = 1
+            for group in best_strategy:
+                for module in group:
+                    self.module_to_group[module] = idx
+                idx += 1
+        
+        # Créer le graphe des dépendances entre groupes
         self.grouped_graph = nx.DiGraph()
-        for group_id in range(1, idx):
+        for group_id in range(1, len(set(self.module_to_group.values())) + 1):
             self.grouped_graph.add_node(group_id)
+        
         for source, target in self.dependency_graph.edges():
             g_s = self.module_to_group.get(source)
             g_t = self.module_to_group.get(target)
             if g_s and g_t and g_s != g_t:
                 self.grouped_graph.add_edge(g_s, g_t)
-    def split_community(self, community: Set[str], granularity: int) -> List[Set[str]]:
-        if len(community) <= granularity:
-            return [community]
-        subgraph = self.dependency_graph.subgraph(community).to_undirected()
-        try:
-            left, right = nx.algorithms.community.kernighan_lin_bisection(subgraph)
-        except:
-            return [community]
-        return self.split_community(set(left), granularity) + self.split_community(set(right), granularity)
+    def _group_by_internal_deps(self, granularity: int) -> Tuple[List[List[str]], float]:
+        """
+        Groupe les modules selon leurs dépendances internes pour obtenir 'granularity' groupes.
+        
+        Args:
+            granularity: Nombre cible de groupes
+            
+        Returns:
+            Tuple contenant les groupes formés et un score d'évaluation (plus petit = meilleur)
+        """
+        undirected = self.dependency_graph.to_undirected()
+        
+        # Détecter des communautés initiales avec modularité
+        initial_communities = list(nx.algorithms.community.greedy_modularity_communities(undirected))
+        
+        # Fusionner ou diviser jusqu'à obtenir granularity groupes
+        if len(initial_communities) > granularity:
+            # Fusionner les communautés les plus similaires
+            while len(initial_communities) > granularity:
+                # Calculer la similarité entre communautés (basée sur les connexions)
+                similarity_scores = []
+                for i in range(len(initial_communities)):
+                    for j in range(i+1, len(initial_communities)):
+                        comm1 = initial_communities[i]
+                        comm2 = initial_communities[j]
+                        edges_between = sum(1 for u in comm1 for v in comm2 if undirected.has_edge(u, v))
+                        score = edges_between / (len(comm1) * len(comm2)) if len(comm1) * len(comm2) > 0 else 0
+                        similarity_scores.append((score, i, j))
+                
+                # Fusionner les deux communautés les plus similaires
+                if similarity_scores:
+                    similarity_scores.sort(reverse=True)
+                    _, i, j = similarity_scores[0]
+                    new_community = initial_communities[i].union(initial_communities[j])
+                    initial_communities = [comm for idx, comm in enumerate(initial_communities) if idx != i and idx != j]
+                    initial_communities.append(new_community)
+                else:
+                    break
+        
+        elif len(initial_communities) < granularity:
+            # Diviser les communautés les plus grandes
+            while len(initial_communities) < granularity:
+                # Trouver la plus grande communauté
+                sizes = [len(comm) for comm in initial_communities]
+                if max(sizes) <= 1:  # Ne peut plus diviser
+                    break
+                
+                largest_idx = sizes.index(max(sizes))
+                largest_comm = initial_communities[largest_idx]
+                
+                if len(largest_comm) > 1:
+                    # Diviser en utilisant bisection
+                    subgraph = self.dependency_graph.subgraph(largest_comm).to_undirected()
+                    try:
+                        left, right = nx.algorithms.community.kernighan_lin_bisection(subgraph)
+                        initial_communities.pop(largest_idx)
+                        initial_communities.append(set(left))
+                        initial_communities.append(set(right))
+                    except:
+                        break
+                else:
+                    break
+        
+        # Évaluer l'équilibre des groupes
+        avg_size = sum(len(comm) for comm in initial_communities) / len(initial_communities) if initial_communities else 0
+        size_variance = sum((len(comm) - avg_size)**2 for comm in initial_communities) / len(initial_communities) if initial_communities else float('inf')
+        
+        return [list(comm) for comm in initial_communities], size_variance
+    
+    def _group_by_external_deps(self, granularity: int) -> Tuple[List[List[str]], float]:
+        """
+        Groupe les modules selon leurs dépendances externes communes.
+        
+        Args:
+            granularity: Nombre cible de groupes
+            
+        Returns:
+            Tuple contenant les groupes formés et un score d'évaluation (plus petit = meilleur)
+        """
+        # Créer des groupes initiaux basés sur les dépendances externes
+        ext_dep_groups = defaultdict(list)
+        for module, deps in self.dependencies.items():
+            # Utiliser un ensemble figé (frozenset) des dépendances externes comme clé
+            ext_deps_key = frozenset(deps['external'])
+            ext_dep_groups[ext_deps_key].append(module)
+        
+        # Convertir en liste de groupes
+        initial_groups = list(ext_dep_groups.values())
+        
+        # Ajuster le nombre de groupes pour atteindre granularity
+        if len(initial_groups) > granularity:
+            # Fusionner les groupes les plus similaires
+            while len(initial_groups) > granularity:
+                similarity_scores = []
+                for i in range(len(initial_groups)):
+                    for j in range(i+1, len(initial_groups)):
+                        # Calculer similarité basée sur dépendances externes communes
+                        group1_modules = set(initial_groups[i])
+                        group2_modules = set(initial_groups[j])
+                        
+                        group1_ext_deps = set()
+                        for module in group1_modules:
+                            group1_ext_deps.update(self.dependencies[module]['external'])
+                            
+                        group2_ext_deps = set()
+                        for module in group2_modules:
+                            group2_ext_deps.update(self.dependencies[module]['external'])
+                        
+                        # Indice de Jaccard pour mesurer la similarité
+                        if not group1_ext_deps and not group2_ext_deps:
+                            similarity = 1.0  # Les deux n'ont pas de dépendances externes
+                        else:
+                            intersection = len(group1_ext_deps.intersection(group2_ext_deps))
+                            union = len(group1_ext_deps.union(group2_ext_deps))
+                            similarity = intersection / union if union > 0 else 0
+                        
+                        similarity_scores.append((similarity, i, j))
+                
+                if similarity_scores:
+                    # Fusionner les deux groupes les plus similaires
+                    similarity_scores.sort(reverse=True)
+                    _, i, j = similarity_scores[0]
+                    merged_group = initial_groups[i] + initial_groups[j]
+                    initial_groups = [grp for idx, grp in enumerate(initial_groups) if idx != i and idx != j]
+                    initial_groups.append(merged_group)
+                else:
+                    break
+        
+        elif len(initial_groups) < granularity:
+            # Diviser les groupes les plus grands
+            while len(initial_groups) < granularity:
+                sizes = [len(grp) for grp in initial_groups]
+                if max(sizes) <= 1:  # Ne peut plus diviser
+                    break
+                
+                largest_idx = sizes.index(max(sizes))
+                largest_group = initial_groups[largest_idx]
+                
+                if len(largest_group) > 1:
+                    # Diviser en deux de manière équilibrée
+                    mid = len(largest_group) // 2
+                    initial_groups.pop(largest_idx)
+                    initial_groups.append(largest_group[:mid])
+                    initial_groups.append(largest_group[mid:])
+                else:
+                    break
+        
+        # Évaluer l'équilibre des dépendances externes
+        ext_dep_per_group = []
+        for group in initial_groups:
+            group_ext_deps = set()
+            for module in group:
+                group_ext_deps.update(self.dependencies[module]['external'])
+            ext_dep_per_group.append(len(group_ext_deps))
+        
+        avg_ext_deps = sum(ext_dep_per_group) / len(ext_dep_per_group) if ext_dep_per_group else 0
+        ext_deps_variance = sum((deps - avg_ext_deps)**2 for deps in ext_dep_per_group) / len(ext_dep_per_group) if ext_dep_per_group else float('inf')
+        
+        # Également évaluer l'équilibre en taille
+        sizes = [len(grp) for grp in initial_groups]
+        avg_size = sum(sizes) / len(sizes) if sizes else 0
+        size_variance = sum((size - avg_size)**2 for size in sizes) / len(sizes) if sizes else float('inf')
+        
+        # Score global (pondéré entre équilibre de taille et équilibre de dépendances)
+        score = 0.5 * size_variance + 0.5 * ext_deps_variance
+        
+        return initial_groups, score
+    
+    def _group_by_hierarchy(self, granularity: int) -> Tuple[List[List[str]], float]:
+        """
+        Groupe les modules selon une hiérarchie: outils de base (peu de dépendances) 
+        versus outils avancés (plus de dépendances).
+        
+        Args:
+            granularity: Nombre cible de groupes
+            
+        Returns:
+            Tuple contenant les groupes formés et un score d'évaluation (plus petit = meilleur)
+        """
+        all_modules = list(self.dependency_graph.nodes())
+        
+        # Calculer le "score de base" pour chaque module
+        # (plus le score est faible, plus c'est un module de base)
+        base_scores = {}
+        for module in all_modules:
+            # Un module de base a peu de dépendances internes entrantes mais potentiellement beaucoup sortantes
+            in_degree = self.dependency_graph.in_degree(module)
+            out_degree = self.dependency_graph.out_degree(module)
+            
+            # Score qui favorise les modules avec plus de dépendances sortantes que entrantes
+            if in_degree == 0:
+                base_scores[module] = -out_degree  # Les modules sans dépendances entrantes sont les plus "basiques"
+            else:
+                ratio = out_degree / in_degree if in_degree > 0 else out_degree
+                base_scores[module] = -ratio  # Négatif car nous voulons trier par ordre croissant
+        
+        # Trier les modules par score (du plus basique au plus avancé)
+        sorted_modules = sorted(all_modules, key=lambda m: base_scores[m])
+        
+        # Diviser en granularity groupes de taille égale
+        groups = []
+        target_size = len(sorted_modules) // granularity
+        remainder = len(sorted_modules) % granularity
+        
+        start_idx = 0
+        for i in range(granularity):
+            # Distribuer le reste pour avoir des groupes plus équilibrés
+            group_size = target_size + (1 if i < remainder else 0)
+            if group_size > 0:  # S'assurer qu'on ne crée pas de groupes vides
+                groups.append(sorted_modules[start_idx:start_idx + group_size])
+                start_idx += group_size
+            
+            # Si on a utilisé tous les modules, sortir de la boucle
+            if start_idx >= len(sorted_modules):
+                break
+        
+        # Éliminer les groupes vides
+        groups = [g for g in groups if g]
+        
+        # Évaluer l'équilibre des groupes
+        sizes = [len(grp) for grp in groups]
+        avg_size = sum(sizes) / len(sizes) if sizes else 0
+        size_variance = sum((size - avg_size)**2 for size in sizes) / len(sizes) if sizes else float('inf')
+        
+        # Évaluer la cohérence hiérarchique des groupes (les modules de base ensemble, les avancés ensemble)
+        hierarchy_variance = 0
+        for group in groups:
+            group_scores = [base_scores[module] for module in group]
+            avg_score = sum(group_scores) / len(group_scores) if group_scores else 0
+            hierarchy_variance += sum((score - avg_score)**2 for score in group_scores) / len(group_scores) if group_scores else 0
+        hierarchy_variance /= len(groups) if groups else 1
+        
+        # Score global
+        score = 0.7 * size_variance + 0.3 * hierarchy_variance
+        
+        return groups, score
     def suggest_packages(self) -> Dict[str, Any]:
         condensed_graph: nx.DiGraph = nx.condensation(self.dependency_graph)
         base_packages: List[str] = self.module_groups['standalone']
--- DependencyAnalyser/frontend.py
+++ DependencyAnalyser/frontend.py
@@ -25,6 +25,71 @@ with st.sidebar:
         groups = set(analyzer.module_to_group.values())
         st.sidebar.write(f"Nombre de sous-librairies détectées: {len(groups)}")
 
+# Fonction pour regrouper hiérarchiquement les modules
+def group_modules_hierarchically(modules, project_name):
+    # Dictionnaire pour stocker la structure hiérarchique
+    hierarchy = {}
+    
+    # Construction de la structure hiérarchique
+    for module in modules:
+        # Supprimer la duplication du nom du projet au début
+        if module.startswith(f"{project_name}.{project_name}."):
+            # Retirer le premier [project_name]. du module
+            module = module[len(project_name)+1:]
+        
+        parts = module.split('.')
+        current_level = hierarchy
+        for i, part in enumerate(parts):
+            # Création du chemin jusqu'à ce niveau
+            path_to_here = '.'.join(parts[:i+1])
+            
+            # Initialisation du niveau s'il n'existe pas
+            if part not in current_level:
+                current_level[part] = {
+                    'full_path': path_to_here,
+                    'children': {},
+                    'is_leaf': (i == len(parts) - 1)
+                }
+            elif i == len(parts) - 1:
+                # Mise à jour si c'est une feuille
+                current_level[part]['is_leaf'] = True
+                
+            # Progression au niveau suivant
+            current_level = current_level[part]['children']
+    
+    return hierarchy
+
+# Fonction pour afficher la structure hiérarchique dans Streamlit avec indentation
+def display_hierarchy(hierarchy, indent=0, parent_path="", project_name=""):
+    # Trier les clés pour un affichage cohérent
+    sorted_keys = sorted(hierarchy.keys())
+    
+    for key in sorted_keys:
+        node = hierarchy[key]
+        full_path = node['full_path']
+        children = node['children']
+        is_leaf = node['is_leaf']
+        
+        # Déterminer quel texte afficher
+        if children:  # Nœud avec enfants
+            # Simplifier l'affichage en utilisant *
+            display_text = f"{full_path}.*"
+            st.markdown("&nbsp;" * (indent * 2) + f"- **{display_text}**")
+            
+            # Afficher les enfants avec indentation supplémentaire
+            display_hierarchy(children, indent + 1, full_path, project_name)
+        elif is_leaf:  # Feuille sans enfants
+            # Afficher le module individuel si ce n'est pas déjà couvert par un parent
+            if not parent_path or not full_path.startswith(parent_path):
+                # Utiliser *.module_name si le module est profondément niché
+                if parent_path:
+                    # Extraire le dernier segment du chemin complet
+                    last_segment = full_path.split('.')[-1]
+                    display_text = f"*.{last_segment}"
+                else:
+                    display_text = full_path
+                st.markdown("&nbsp;" * (indent * 2) + f"- *{display_text}*")
+
 # Affichage des résultats dans la fenêtre principale
 if analyzer and groups:
     # Affichage du graphe de dépendances
@@ -40,12 +105,15 @@ if analyzer and groups:
     plt.axis('off')
     st.pyplot(fig)
     
-    # Affichage des détails des sous-librairies
+    # Affichage des détails des sous-librairies avec regroupement hiérarchique
     st.header("Détails des sous-librairies")
-    cols = st.columns(min(3, len(groups)))
-    for i, g_id in enumerate(sorted(list(groups))):
-        with cols[i % len(cols)]:
-            members = [m for m, grp in analyzer.module_to_group.items() if grp == g_id]
-            st.markdown(f"### Sous-librairie {g_id}")
-            for m in sorted(members):
-                st.write(m)
+    
+    # Présentation sur une seule colonne avec éléments collapsables (fermés par défaut)
+    for g_id in sorted(list(groups)):
+        members = [m for m, grp in analyzer.module_to_group.items() if grp == g_id]
+        
+        # Créer un élément expansible (fermé par défaut)
+        with st.expander(f"Sous-librairie {g_id}", expanded=False):
+            # Utilisation du regroupement hiérarchique pour l'affichage
+            hierarchy = group_modules_hierarchically(sorted(members), project_name)
+            display_hierarchy(hierarchy, project_name=project_name)
