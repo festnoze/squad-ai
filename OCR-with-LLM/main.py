c2b232f2dc7bf5f25614c57555362cacb2c8cacc
étienne <etienne.millerioux@studi.fr>
2025-05-27 00:04:25 +0200

    remove previous folder upon running

--- OCR-with-LLM/main.py
+++ OCR-with-LLM/main.py
@@ -5,6 +5,10 @@ parser: argparse.ArgumentParser = argparse.ArgumentParser()
 parser.add_argument("--folder", default="images_to_ocr", help="Folder containing images to process")
 args = parser.parse_args()
 
+# Remove "html" folder if it exists
+import shutil
+shutil.rmtree("html", ignore_errors=True)
+
 title = "La Stratégie des Douze Points Magiques de Dr Tan"
 OCRFolderImages(args.folder).process_all_folder_images(title)
 #OCRFolderImages(args.folder)._build_index()
